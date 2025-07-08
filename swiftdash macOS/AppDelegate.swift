d5df945b93d65033db97d5bcc4e4481fdc6e021d
Krzysztof Furman <k_furman@outlook.com>
2025-07-08 22:56:14 +0100

    Initial Commit

---
+++ swiftdash macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  swiftdash macOS
+//
+//  Created by Kris Furman on 08/07/2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
